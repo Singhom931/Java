880c840b0806a406e744251a94946737523731a9
Singhom931 <75739647+Singhom931@users.noreply.github.com>
2022-10-13 18:08:41 +0530

    Add files via upload

---
+++ lab day/5/ThrowThrows.java
@@ -0,0 +1,18 @@
+public class ThrowThrows  
+{  
+    static void zero() throws ArithmeticException  
+    {   
+        throw new ArithmeticException("throw ArithmeticException");  
+    }  
+    public static void main(String args[])  
+    {  
+        try  
+        {  
+            zero();  
+        }  
+        catch(ArithmeticException e)  
+        {  
+            System.out.println(e);  
+        }  
+    }  
+}  
